dade89fdeec8d9ae4460d387bbfde364a4aa1f9a
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-05-03 12:58:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
-
-# Load the trained model
-MODEL_PATH = "model.savedmodel"
-model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 import os
 
 print("Current working directory:", os.getcwd())
