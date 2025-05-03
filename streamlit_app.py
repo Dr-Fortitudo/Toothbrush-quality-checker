e111458c133f4764189a93a713e5eed48ffd456c
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-05-03 13:41:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
+import streamlit as st
 
 model_path = "model.savedmodel"
 loaded = tf.saved_model.load(model_path)
 
-# List available input keys
-print(loaded.signatures["serving_default"].structured_input_signature)
+# Display input signature in Streamlit
+signature = loaded.signatures["serving_default"].structured_input_signature
+st.write("Model Input Signature:", signature)
