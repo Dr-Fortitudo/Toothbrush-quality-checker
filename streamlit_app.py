bfe2dbec0ee985e203fa4cde5c0426f2917da102
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-05-03 13:24:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,13 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
-import tensorflow as tf
+from keras.layers import TFSMLayer
+from keras import Sequential
 
-MODEL_PATH = "saved_model.pb"
+# Load the trained model
+MODEL_PATH = "model.savedmodel"
+layer = TFSMLayer(MODEL_PATH, call_endpoint="serving_default")
+model = Sequential([layer])
+
 loaded = tf.saved_model.load(MODEL_PATH)
 print(loaded.signatures.keys())
